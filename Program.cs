0d6b78e62afb19dda7e91c4e6a63093997f4c82b
Davit <dav0709@icloud.com>
2023-03-22 23:36:17 +0400

    uxagrum

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿double[] numbers = { 3, 7, 22, 2, 78 };
+
+double min = numbers[0];
+double max = numbers[0];
+
+foreach (double number in numbers)
+{
+    if (number < min)
+    {
+        min = number;
+    }
+
+    if (number > max)
+    {
+        max = number;
+    }
+}
+
+double difference = max - min;
+
+Console.WriteLine("Максимальный элемент: " + max);
+Console.WriteLine("Минимальный элемент: " + min);
+Console.WriteLine("Разница: " + difference);
